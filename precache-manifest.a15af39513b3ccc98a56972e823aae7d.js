6334b5eadbbb7bdc217777592dd1d5e47a879ecd
Aline Brito <14023536+alinebrito@users.noreply.github.com>
2022-03-18 20:34:01 -0300

    deploy

---
+++ precache-manifest.a15af39513b3ccc98a56972e823aae7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b959aef1b3342d317b415e1502454547",
+    "url": "/refgraph-fowler-study/index.html"
+  },
+  {
+    "revision": "c8a1670bf1dd4d46fd2e",
+    "url": "/refgraph-fowler-study/static/css/main.705c4685.chunk.css"
+  },
+  {
+    "revision": "0c9289e344e10e1d5c3c",
+    "url": "/refgraph-fowler-study/static/js/2.992dffca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/refgraph-fowler-study/static/js/2.992dffca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a1670bf1dd4d46fd2e",
+    "url": "/refgraph-fowler-study/static/js/main.76bca0ba.chunk.js"
+  },
+  {
+    "revision": "dbeb4c902874c6a85aa5",
+    "url": "/refgraph-fowler-study/static/js/runtime-main.d0a68fca.js"
+  }
+]);
